c7b4d67540e1aab5877fad069cfbec6dfa7df223
Ian Logan <itlogan9@gmail.com>
2020-04-08 16:24:01 -0500

    Finish mergesort

--- mergesort.js
+++ mergesort.js
@@ -1,33 +1,35 @@
+/* eslint no-console: "off" */
 function split(wholeArray) {
-	const midPoint = Math.ceil(wholeArray.length / 2);
-	const firstHalf = wholeArray.slice(0, midPoint);
-	const secondHalf = wholeArray.slice(midPoint);
+  const midPoint = Math.ceil(wholeArray.length / 2);
+  const firstHalf = wholeArray.slice(0, midPoint);
+  const secondHalf = wholeArray.slice(midPoint);
 
-	return [ firstHalf, secondHalf ];
+  return [firstHalf, secondHalf];
 }
 
 function merge(firstArray, secondArray) {
-	const mergedArray = [];
-
-	while (firstArray.length || secondArray.length) {
-		if (firstArray[0] < secondArray[0]) {
-			mergedArray.push(firstArray.shift());
-		} else {
-			mergedArray.push(secondArray.shift());
-		}
-	}
-	return mergedArray;
+  const mergedArray = [];
+	const finalLength = firstArray.length + secondArray.length;
+  while (mergedArray.length !== finalLength) {
+    if (firstArray[0] < secondArray[0] || !secondArray[0]) {
+      mergedArray.push(firstArray.shift());
+    } else {
+      mergedArray.push(secondArray.shift());
+    }
+  }
+  console.log('MERGE FN OUTPUT: ', mergedArray);
+  return mergedArray;
 }
 
 function mergeSort(array) {
-	console.log('array:', array);
-	const sortedList = [];
-	if (array.length === 1) return array;
+  console.log('array:', array);
+  if (array.length === 1) return array;
 
-	const [ firstHalf, secondHalf ] = split(array);
-	mergeSort(firstHalf); // recursively run
-	mergeSort(secondHalf); // run after first finishes
-	// return merge(firstHalf, secondHalf);
+  const [firstHalf, secondHalf] = split(array);
+  const first = mergeSort(firstHalf); // recursively run
+  const second = mergeSort(secondHalf); // run after first finishes
+  console.log('first: ', first, 'second: ', second);
+  return merge(first, second);
 }
 
-mergeSort([ 5, 2, 4, 3, 1 ]);
+mergeSort([5, 2, 4, 3, 1, 9, 11, 3]);
